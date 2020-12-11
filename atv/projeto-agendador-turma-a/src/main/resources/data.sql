f2dd18a983a4b8f14c985d67edddddbac1a06c75
DJuncks <53724000+danieljuncks@users.noreply.github.com>
2020-12-11 15:11:09 -0300

    atv disciplina organizado

--- atv/projeto-agendador-turma-a/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-insert into cliente (id, nome, dia_nascimento, mes_nascimento)
-values
-(1, 'zé 1', 26, 11),
-(2, 'maria 2', 26, 11),
-(3, 'joana 3', 26, 11),
-(4, 'joana 3', 16, 11),
-(5, 'joana 3', 16, 11);
--- atv/projeto-jpa4/src/main/java/br/com/bandtec/projetojpa4/visoes/FabricanteSimples.java
+++
@@ -1,7 +0,0 @@
-package br.com.bandtec.projetojpa4.visoes;
-
-public interface FabricanteSimples {
-
-    String getNome();
-
-}
